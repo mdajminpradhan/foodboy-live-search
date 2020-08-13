0731706424f65cb9f114c2c0c245a8cab07db53d
Ajmin Pradhan <md.ajminpradhan@outlook.com>
2020-08-13 17:55:28 +0800

    Updates

---
+++ precache-manifest.ec4c81ff77a48915c46c4462938921e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "780f7449aa57dce1374e7d7f3ec0e3a9",
+    "url": "/foodboy-live-search/index.html"
+  },
+  {
+    "revision": "22a7ff67ecc0f3cfdaba",
+    "url": "/foodboy-live-search/static/css/main.63508688.chunk.css"
+  },
+  {
+    "revision": "d40fabac01e22ac05a9c",
+    "url": "/foodboy-live-search/static/js/2.9ff326f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/foodboy-live-search/static/js/2.9ff326f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22a7ff67ecc0f3cfdaba",
+    "url": "/foodboy-live-search/static/js/main.7dc66d47.chunk.js"
+  },
+  {
+    "revision": "6988c5610fb1af939332",
+    "url": "/foodboy-live-search/static/js/runtime-main.f5eec35e.js"
+  }
+]);
